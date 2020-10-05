73116a1ebe9736f4d7931f87ac5edd45ca1f0243
Acacia Calvin <acaciacalvin.ac15.AC@gmail.com>
2020-10-04 19:53:32 -0500

    Expose details of movie (ratings using RatingBar, popularity, and synopsis) in a separate activity. Allow video posts to be played in full-screen using the YouTubePlayerView

--- app/src/main/java/adapters/MovieAdapter.java
+++ app/src/main/java/adapters/MovieAdapter.java
@@ -1,19 +1,26 @@
 package adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.os.Parcel;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.R;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -57,6 +64,8 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        RelativeLayout container;
+
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -66,8 +75,9 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview= itemView.findViewById(R.id.tvOverview);
             ivPoster= itemView.findViewById(R.id.ivPoster);
+            container= itemView.findViewById(R.id.container);
         }
-        public void bind (Movie movie){
+        public void bind (final Movie movie){
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
 
@@ -85,6 +95,16 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
 
 
             Glide.with(context).load(imageUrl).into (ivPoster);
+                //1. Register click Listener on the whole row
+             container.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //2. Navigate to a new activity on tap
+                        Intent i= new Intent(context, DetailActivity.class);
+                        i.putExtra("movie", Parcels.wrap(movie));
+                        context.startActivity(i);
+                    }
+                });
         }
     }
 }
